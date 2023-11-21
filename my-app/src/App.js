32cf8946e9f70737dd601de4b6dfb1f6f843f93c
anu-bhati <anukumari070702@gmail.com>
2023-11-21 15:20:01 +0530

    Working on it

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,17 +1,46 @@
 
 import './App.css';
-import Navbar from './Components/Navbar' ; 
-import Textarea from './Components/Textarea' ;
+import Navbar from './Components/Navbar' ;  
+import Alert from './Components/Alert' ;
+import Textarea from './Components/Textarea' ; 
+import React , {useState} from 'react'
+//import About from './Components/About' ;  
 
-function App() {
+function App() { 
+
+  const [mode , setMode] = useState("light") 
+  const [alert , setAlert] = useState("null")  
+  
+  const showAlert = (message , type)=>{ 
+       setAlert({ 
+          msg : message , 
+          type : type 
+       }) 
+       setTimeout(() => {
+        setAlert(null);
+    }, 1500);
+  } 
+
+  const toggleMode = ()=>{ 
+    if(mode === 'light'){ 
+      setMode('dark');  
+      document.body.style.backgroundColor = '#042743'; 
+      showAlert("Dark mode has been enabled", "success"); 
+    }
+    else{ 
+     setMode('light') ;  
+     document.body.style.backgroundColor = 'white'; 
+     showAlert("Light mode has been enabled", "success");  
+    }
+  }
   return (
    <> 
-   
-   <Navbar title = "Textutils" about = "About Us" /> 
+   <Navbar title = "Textutils" about = "About Us" mode = {mode} toggleMode = {toggleMode} /> 
+   <Alert alert = {alert}/>
    <div className = "container my-3"> 
-	 <Textarea heading = "Enter the text to analyze" />
+	 <Textarea heading = "Enter the text to analyze" mode = {mode} showAlert = {showAlert} />  
+   {/*<About/>*/}
 	 </div> 
-  
    </>
   );
 }
--- my-app/src/Components/Textarea.js
+++ my-app/src/Components/Textarea.js
@@ -4,29 +4,38 @@ export default function Textarea(props) {
 	const handleUpClick = ()=>{ 
        //console.log("Uppercase was clicked" , + text) ; 
 	   let newText = text.toUpperCase() ;  
-	   setText(newText) ; 
+	   setText(newText) ;  
+	   props.showAlert("Converted to uppercase!", "success");
 	} 
 	const handleLowClick = ()=>{ 
 		//console.log("Lowercase was clicked" , + text) ; 
 		let newText = text.toLowerCase() ;  
 		setText(newText) ; 
+		props.showAlert("Converted to lowercase!", "success");
 	 }
 	const handleOnChange = (event)=>{ 
 		//console.log("On change") ; 
 		setText(event.target.value) ; 
-	}
+	} 
+	const clearText = ()=>{ 
+		//console.log("Uppercase was clicked" , + text) ; 
+		let newText = '' ;  
+		setText(newText) ; 
+		props.showAlert("Text cleared!", "success");
+	 } 
 	const [text , setText] = useState('Enter text here') ;
   return ( 
 	<>
-	<div className = "container">
-	    <h1>{props.heading} </h1>
+	<div className = "container" style={{color: props.mode==='dark'?'white':'#042743'}}>
+	    <h1 className = "mb-4">{props.heading} </h1>
         <div className="mb-3">
-        <textarea className="form-control" value = {text} onChange = {handleOnChange} id="my-Box" rows="8"></textarea>
+        <textarea className="form-control" value = {text} onChange = {handleOnChange} style={{color: props.mode==='dark'?'white':'#042743' , backgroundColor: props.mode==='dark'?'#13466e':'white'}} id="my-Box" rows="8"></textarea>
        </div> 
 	   <button className="btn btn-primary mx-2"  onClick = {handleUpClick}>Convert to Uppercase</button> 
-	   <button className="btn btn-primary mx-2"  onClick = {handleLowClick}>Convert to Lowercase</button>
+	   <button className="btn btn-primary mx-2"  onClick = {handleLowClick}>Convert to Lowercase</button> 
+	   <button className="btn btn-primary mx-2"  onClick = {clearText}>Clear Text</button>
 	</div>  
-	<div className="container my-3">
+	<div className="container my-3" style={{color: props.mode==='dark'?'white':'#042743'}}>
      <h2>Your text Summary </h2> 
 	 <p>{text.split(" ").length} words and {text.length} characters</p> 
 	 <p>{0.008 * text.split(" ").length} minutes read</p> 
